d0cbb4569d08f863c3a9e311ad20f55518d2cb31
nsforneris <168025146+nsforneris@users.noreply.github.com>
2026-07-13 11:12:14 +0200

    Create run_zooroh_wgs.sh

---
+++ code/DAMONA/WGS/run_zooroh_wgs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=wgs
+#SBATCH --time=10:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --partition=batch
+#SBATCH --array=1-11
+#SBATCH --output=wgs.out
+
+module load R
+
+echo $SLURM_JOB_ID
+
+wdir='/scratch/ulg/genan/forneris/eval'
+
+fi="seq_evalset_gen.txt.gz"
+
+SCRATCH=$LOCALSCRATCH/$SLURM_JOB_ID
+mkdir -p $SCRATCH
+cd $SCRATCH
+
+cp $wdir/seq/run_zooroh_wgs.R           $SCRATCH/.
+cp $wdir/seq/$fi                        $SCRATCH/.
+
+/usr/bin/time -v Rscript run_zooroh_wgs.R $fi $SLURM_ARRAY_TASK_ID
+
+# remove temporary files
+rm -rf $SCRATCH
